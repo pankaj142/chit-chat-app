b003a3e15e02fc77ac7b8d4546516081c5bba35d
Pankaj Agham <pankajagham1@gmail.com>
2019-02-26 20:56:27 +0530

    initial commit- express server setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const PublicPath = path.join(__dirname, '../public')
+
+//middlewares
+app.use(express.static(PublicPath));
+
+//routes
+// app.get('/', (req,res)=>{
+//     res.sendFile('index.html');
+// })
+
+app.listen(3000, ()=>{
+    console.log(`Server is Up on port ${port}`);
+});
